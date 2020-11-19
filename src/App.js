04d60068f58b3146f658ae2ff77e9f9b25559e5c
Munir Mahmud <munirmahmud6@gmail.com>
2020-11-19 10:25:46 +0600

    Home page banner added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Home from './pages/HomePage/Home';
 
 const App = () => {
     return (
         <div>
             <Header />
+            <Home />
         </div>
     );
 };
---
+++ src/pages/HomePage/Home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Home.styles.css';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img className="home__banner" src="../images/amazon-home-banner.jpg" alt="Large Banner"/>
+
+                <div className="home__row">
+                    
+                </div>
+                <div className="home__row">
+
+                </div>
+                <div className="home__row">
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
