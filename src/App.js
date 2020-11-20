b0131b48e9d9d153f58cdd4d0083eeee5e678217
Munir Mahmud <munirmahmud6@gmail.com>
2020-11-20 15:38:53 +0600

    Router defined for home & checkout page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './pages/HomePage/Home';
 
 const App = () => {
     return (
-        <div>
-            <Header />
-            <Home />
+        <div className="app">
+            <Router>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/checkout" />
+                </Switch>
+            </Router>
         </div>
     );
 };
